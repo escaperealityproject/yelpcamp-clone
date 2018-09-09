2a240d69f003afaba2faae557a552a899797cd6d
aniket <escaperealityproject@gmail.com>
2018-09-09 20:57:30 +0530

    Toggle login logout, added security to comment routes

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+
 //ROUTES
 
 app.get("/", function(req, res) {
@@ -95,7 +100,7 @@ app.get("/campgrounds/:id", function(req, res) {
 //COMMENTS ROUTES
 //===============================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -107,7 +112,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     })
 })
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err)
@@ -153,7 +158,20 @@ app.get("/login", function(req, res) {
 app.post("/login", passport.authenticate("local", {
     successRedirect: "/campgrounds",
     failureRedirect: "/login"
-}), function(req, res) {})
+}), function(req, res) {});
+
+//logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function() {
     console.log("Server is running");
